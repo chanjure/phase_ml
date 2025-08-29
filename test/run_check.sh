d9bc9f854713601653c054d8d498f6bf5553478b
chanjure <chanjure@snu.ac.kr>
2025-08-29 19:07:22 +0100

    fixed typo in nbstrip

--- test/run_check.sh
+++ test/run_check.sh
@@ -1,6 +1,5 @@
 jupyter nbconvert --to notebook --execute src/notebooks/empirical_phase.ipynb
-mv scripts/SRBM-scaling_analysis.nbconvert.ipynb src/notebooks/empirical_phase.ipynb
-nbstripout src/notebook/empirical_phase.ipynb        
-
+mv src/notebooks/empirical_phase.nbconvert.ipynb src/notebooks/empirical_phase.ipynb
+nbstripout src/notebooks/empirical_phase.ipynb        
 
 python -m pytest -v --cov-config=.coveragerc --cov=./ test/
