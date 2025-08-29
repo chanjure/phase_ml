8d2c528629e5d45c4199cc9d480c72eb221f3e0a
chanjure <chanjure@snu.ac.kr>
2025-08-29 18:15:32 +0100

    added a test

---
+++ .github/workflows/run_check.sh
@@ -0,0 +1,8 @@
+jupyter nbconvert --to notebook --execute src/notebooks/empirical_phase.ipynb
+mv scripts/SRBM-scaling_analysis.nbconvert.ipynb src/notebooks/empirical_phase.ipynb
+nbstripout src/notebook/empirical_phase.ipynb
+
+rm -rf ./figures/*
+
+python -m pytest -v --cov-config=.coveragerc --cov=./ test/
+rm -rf ./figures/*
